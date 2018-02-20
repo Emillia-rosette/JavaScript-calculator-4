b07fcf0231b47dc5c41c25b8dc002440a58066e6
Tech-A66 <tech-a66@MacBook-Air-10.local>
2018-02-20 01:41:19 +0000

    second page created

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 //Emillia-rosette Nlandu
 //write a command line calculator application that has two settings, (b)asic and (a)dvanced.
 // from textBoxes.html
-
+document.getElementById("myButton").onclick = function () {
+        alert("hello");
+    }
 
 //var hello = prompt("What is your name"); //allow user to input their name
 
 
-/
+//
 //}
 
 
