8999fd26e64e22267197aab27983be8fc0617cab
Tech-A66 <tech-a66@MacBook-Air-10.local>
2018-02-20 01:27:06 +0000

    form created

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//Emillia-rosette Nlandu
+//write a command line calculator application that has two settings, (b)asic and (a)dvanced.
+// from textBoxes.html
+
+
+//var hello = prompt("What is your name"); //allow user to input their name
+
+
+/
+//}
+
+
+
+//}
