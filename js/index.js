c02439d61ec8124a0905adfb2be1f11ed19145d1
Tech-A66 <tech-a66@MacBook-Air-10.local>
2018-02-20 02:28:12 +0000

    php page created

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 //Emillia-rosette Nlandu
 //write a command line calculator application that has two settings, (b)asic and (a)dvanced.
 // from textBoxes.html
-document.getElementById("myButton").onclick = function () {
-        alert("hello");
-    }
 
 //var hello = prompt("What is your name"); //allow user to input their name
+var person;
 
-
-//
-//}
-
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+}
 
 
 //}
